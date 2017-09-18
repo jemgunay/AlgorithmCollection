b162f4ee2d8bfe981b64135182b5135c303ea431
Jem Gunay <jemgunay@gmail.com>
2017-09-18 02:27:20 +0100

    Added example usage

--- AlgorithmCollection.java
+++ AlgorithmCollection.java
@@ -3,9 +3,29 @@ import algorithm.*;
 public class AlgorithmCollection {
 	
 	public static void main(String[] args) {
+		// linked list
+		LinkedList<String> linkedList = new LinkedList<String>();
+		linkedList.add("Example");
+		String result = linkedList.get(0);
+		System.out.println(result);
+
+		// linked list
+		DoublyLinkedList<String> dLinkedList = new DoublyLinkedList<String>();
+		dLinkedList.add("Example");
+		result = dLinkedList.get(0);
+		System.out.println(result);
+		
+		// array list
 		ArrayList<String> arrayList = new ArrayList<String>();
 		arrayList.add("Example");
-		arrayList.add("list");
+		result = arrayList.get(0);
+		System.out.println(result);
+		
+		// stack
+		Stack<String> stack = new Stack<String>();
+		stack.push("Example");
+		result = stack.pop();
+		System.out.println(result);
 		
 		
 	}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# AlgorithmCollection
-A collection of basic practice algorithm implementations
+# Algorithm Collection
+A collection of basic practice algorithm implementations written in Java.
+<br>
+
+Currently consists of LinkedList, DoublyLinkedList, ArrayList and Stack implementations which can be found in the '/src' direectory. JUnit test units have also been developed for each implementationin te '/test' directory. 
